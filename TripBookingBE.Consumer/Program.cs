f3fd515ff0a784adbd2d389fb3d34585aa7fc1f1
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-08-30 23:12:40 +0700

    rpc done

--- TripBookingBE.Consumer/Program.cs
+++ TripBookingBE.Consumer/Program.cs
@@ -4,40 +4,67 @@ using System.Text;
 
 internal class Program
 {
+    const string QUEUE_NAME = "rpc_queue";
     private static async Task Main(string[] args)
     {
         var factory = new ConnectionFactory { HostName = "localhost", Port = 6078 };
         using var connection = await factory.CreateConnectionAsync();
         using var channel = await connection.CreateChannelAsync();
 
-        await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Topic);
-        QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
-        string queueName = queueDeclareResult.QueueName;
-        Console.WriteLine($"Instance exclusive queue: {queueName}");
-        foreach (var severity in args)
-        { await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: severity); }
+        await channel.QueueDeclareAsync(queue: QUEUE_NAME, durable: false, exclusive: false,
+    autoDelete: false, arguments: null);
         await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
 
         Console.WriteLine(" [*] Waiting for messages.");
 
         var consumer = new AsyncEventingBasicConsumer(channel);
-        consumer.ReceivedAsync += async (model, ea) =>
+        consumer.ReceivedAsync += async (sender, ea) =>
         {
+            AsyncEventingBasicConsumer cons = (AsyncEventingBasicConsumer)sender;
+            IChannel ch = cons.Channel;
+            string response = string.Empty;
+
             var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine($" [x] Received {message}");
+            IReadOnlyBasicProperties props = ea.BasicProperties;
+            var replyProps = new BasicProperties
+            {
+                CorrelationId = props.CorrelationId
+            };
 
-            int dots = message.Split('.').Length - 1;
-            await Task.Delay(dots * 1000);
-
-            Console.WriteLine(" [x] Done");
-
-            await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+            try
+            {
+                var message = Encoding.UTF8.GetString(body);
+                int n = int.Parse(message);
+                Console.WriteLine($" [.] Fib({message})");
+                response = Fib(n).ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($" [.] {e.Message}");
+                response = string.Empty;
+            }
+            finally
+            {
+                var responseBytes = Encoding.UTF8.GetBytes(response);
+                await ch.BasicPublishAsync(exchange: string.Empty, routingKey: props.ReplyTo!,
+                    mandatory: true, basicProperties: replyProps, body: responseBytes);
+                await ch.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+            }
         };
 
-        await channel.BasicConsumeAsync(queueName, autoAck: false, consumer: consumer);
+        await channel.BasicConsumeAsync(QUEUE_NAME, autoAck: false, consumer: consumer);
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
+    }
+
+    static int Fib(int n)
+    {
+        if (n is 0 or 1)
+        {
+            return n;
+        }
+
+        return Fib(n - 1) + Fib(n - 2);
     }
 }
