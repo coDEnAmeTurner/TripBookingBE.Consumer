c796e73bf482c2a36757af6c94e66e1ca9913399
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-09-01 15:42:42 +0700

    p confirms

--- TripBookingBE.Consumer/Program.cs
+++ TripBookingBE.Consumer/Program.cs
@@ -4,50 +4,39 @@ using System.Text;
 
 internal class Program
 {
-    const string QUEUE_NAME = "rpc_queue";
+    const string QUEUE_NAME = "logs";
     private static async Task Main(string[] args)
     {
         var factory = new ConnectionFactory { HostName = "localhost", Port = 6078 };
         using var connection = await factory.CreateConnectionAsync();
         using var channel = await connection.CreateChannelAsync();
 
-        await channel.QueueDeclareAsync(queue: QUEUE_NAME, durable: false, exclusive: false,
+        await channel.QueueDeclareAsync(queue: QUEUE_NAME, durable: true, exclusive: false,
     autoDelete: false, arguments: null);
         await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
 
         Console.WriteLine(" [*] Waiting for messages.");
 
         var consumer = new AsyncEventingBasicConsumer(channel);
+
         consumer.ReceivedAsync += async (sender, ea) =>
         {
             AsyncEventingBasicConsumer cons = (AsyncEventingBasicConsumer)sender;
             IChannel ch = cons.Channel;
-            string response = string.Empty;
 
             var body = ea.Body.ToArray();
-            IReadOnlyBasicProperties props = ea.BasicProperties;
-            var replyProps = new BasicProperties
-            {
-                CorrelationId = props.CorrelationId
-            };
 
             try
             {
                 var message = Encoding.UTF8.GetString(body);
-                int n = int.Parse(message);
-                Console.WriteLine($" [.] Fib({message})");
-                response = Fib(n).ToString();
+                Console.WriteLine($"Message received: {message}");
             }
             catch (Exception e)
             {
-                Console.WriteLine($" [.] {e.Message}");
-                response = string.Empty;
+                Console.WriteLine($" [.] {e.Message};{e.InnerException?.Message}");
             }
             finally
             {
-                var responseBytes = Encoding.UTF8.GetBytes(response);
-                await ch.BasicPublishAsync(exchange: string.Empty, routingKey: props.ReplyTo!,
-                    mandatory: true, basicProperties: replyProps, body: responseBytes);
                 await ch.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
             }
         };
